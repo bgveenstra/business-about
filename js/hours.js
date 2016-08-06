f7f0da5da36c8585c7e1950a53a747308e2d392e
Brianna Veenstra <b.g.veenstra@gmail.com>
2016-08-06 13:43:32 -0700

    init

---
+++ js/hours.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  // var now = new Date();
+  // var hour = now.getHours();
+  // var minute = now.getMinutes();
+  // var allDays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+  // var day = allDays[now.getDay()];
+  console.log(day);
+});
